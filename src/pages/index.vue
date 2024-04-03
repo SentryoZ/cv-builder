36964bd25ff7bd10c176145a7a670ef88a7e43f8
tAmh <thuynh204@csu.fullerton.edu>
2024-04-03 15:32:57 -0700

    input component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,47 +3,10 @@
     <v-container>
       <div class="v-row">
         <div class="v-col-6">
+          <OptionsField :resumeData="resumeData" @save="saveResumeData" />
           <div class="d-flex flex-column">
-            <div class="flex-1-1">
-              <v-text-field
-                label="Padding X"
-                type="number"
-                v-model="paddingTop"
-                suffix="px"
-                @change="saveResumeData(resumeData)"
-              />
-            </div>
-            <div class="flex-1-1">
-              <v-text-field
-                label="Padding Y"
-                type="number"
-                v-model="paddingBottom"
-                suffix="px"
-                @change="saveResumeData(resumeData)"
-              />
-            </div>
-            <div class="d-flex">
-              <div class="flex-1-1">
-                <v-text-field
-                  label="Text Color"
-                  type="color"
-                  v-model="resumeData.options['text-color']"
-                  @change="saveResumeData(resumeData)"
-                />
-              </div>
-              <div class="flex-1-1">
-                <v-text-field
-                  label="Background Color"
-                  type="color"
-                  v-model="resumeData.options['primary-color']"
-                  @change="saveResumeData(resumeData)"
-                />
-              </div>
-            </div>
-          </div>
-          <div class="d-flex flex-column">
-            <div v-for="(section, index) in resumeData.sections" :key=index>
-              <BlockField :block=section :level=0></BlockField>
+            <div v-for="(section, index) in resumeData.sections" :key="index">
+              <BlockField :block="section" :level="0"></BlockField>
             </div>
           </div>
         </div>
@@ -56,7 +19,11 @@
               inset
             ></v-switch>
             <pre v-if="showDebug"> {{ debugData }}</pre>
-            <preview-resume v-else :resume-data=resumeData :key=updateKey></preview-resume>
+            <preview-resume
+              v-else
+              :resume-data="resumeData"
+              :key="updateKey"
+            ></preview-resume>
           </div>
         </div>
       </div>
@@ -64,18 +31,17 @@
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
 
 <script>
 import initResumeData from "@/config/initResumeData";
 import PreviewResume from "@/components/Builder/PreviewResume.vue";
 import BlockField from "@/components/Builder/InputFields/BlockField.vue";
+import OptionsField from "@/components/Builder/OptionsField.vue";
 
 export default {
   name: "resumeBuilder",
-  components: {BlockField, PreviewResume},
+  components: { BlockField, PreviewResume, OptionsField },
   data() {
     return {
       resumeData: {},
@@ -111,48 +77,19 @@ export default {
       resumeData = this.saveResumeData(initResumeData);
     }
 
-    // // order options in resumeData
-    // let orderedOptions = {
-    //   "x": options["x"] + "px",
-    //   "y": options["y"] + "px",
-    //   "text-color": options["text-color"],
-    //   "primary-color": options["primary-color"],
-    // };
-    // resumeData.options = orderedOptions;
-
     this.resumeData = resumeData;
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     // save resume data to local storage
     saveResumeData(data) {
+      this.resumeData = data;
       localStorage.setItem("resume_data", JSON.stringify(data));
-      this.updateKey++
+      this.updateKey++;
       return data;
     },
   },
   computed: {
-    paddingTop: {
-      // get x value
-      get() {
-        return this.resumeData.options["x"].replace("px", "");
-      },
-      // set x value with px to cvData
-      set(value) {
-        this.resumeData.options["x"] = value + "px";
-      },
-    },
-    paddingBottom: {
-      // get y value
-      get() {
-        return this.resumeData.options["y"].replace("px", "");
-      },
-      // set y value with px to cvData
-      set(value) {
-        this.resumeData.options["y"] = value + "px";
-      },
-    },
     debugData() {
       return JSON.stringify(this.resumeData, null, 2);
     },
